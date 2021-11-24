b3e4f7ad3bf4b8a09eda52095b31867cda91bd26
andy.tsai <andy.tsai@deephow.com>
2021-11-24 20:09:51 +0800

    add scss structure

--- src/App.vue
+++ src/App.vue
@@ -18,17 +18,6 @@ import SideBar from "./views/SideBar.vue";
 </template>
 
 <style>
-:root {
-  --header-heigh: 3rem;
-  --layout-heigh: calc(100vh - var(--header-heigh));
-  --primary-color: #74b3ff;
-}
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-}
-
 #app {
   min-height: 100vh;
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import './scss/main.scss'
 createApp(App).mount('#app')
