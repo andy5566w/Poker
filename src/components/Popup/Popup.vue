d4dad783d9032d9d39871c3b44026d50b81cda5c
andy tsai <andy.tsai@deephow.com>
2021-11-27 16:24:13 +0800

    signIn page

--- src/components/Popup/Popup.vue
+++ src/components/Popup/Popup.vue
@@ -1,26 +1,37 @@
 <template>
   <div :class="style.container">
-    <span @click="handleClick" :class="style.closeIcon"
-      ><ion-icon name="close-circle-outline"></ion-icon
-    ></span>
+    <span @click="handleClick" :class="style.closeIcon">
+      <ion-icon name="close-circle-outline"></ion-icon>
+    </span>
     <div class="container bg-gray-500 p-5 rounded-lg">
       <div class="columns">
         <div class="column">
-          <div class="label">請輸入玩家</div>
-          <input type="text" class="input" v-model="title" />
+          <div class="label">請輸入email</div>
+          <input type="text" class="input" v-model="email" />
+          <div v-if="isEmailValid" :class="style.error">信箱格式錯誤</div>
         </div>
       </div>
 
       <div class="columns">
         <div class="column">
-          <div ref="contenteditable" contenteditable @input="handleInput"></div>
+          <div class="label">請輸入密碼</div>
+          <input type="password" class="input" v-model="password" />
         </div>
-        <div class="column" v-html="html"></div>
       </div>
 
       <div class="columns">
         <div class="column">
-          <button @click="save" class="button is-primary is-pulled">Submit</button>
+          <div class="label">請再次輸入密碼</div>
+          <input type="password" class="input" v-model="secondPassword" />
+          <div v-if="isPasswordCorrespond" :class="style.error">密碼不一致</div>
+        </div>
+      </div>
+
+      <div class="columns">
+        <div class="column">
+          <button @click="handleSignUp" class="button is-primary is-pulled">
+            Submit
+          </button>
         </div>
       </div>
     </div>
@@ -28,8 +39,16 @@
 </template>
 
 <script setup>
+import Swal from "sweetalert2";
+import axios from "axios";
 import style from "./Popup.module.scss";
-import { onMounted, onUnmounted, defineProps, defineEmits } from "@vue/runtime-core";
+import {
+  onMounted,
+  onUnmounted,
+  defineProps,
+  defineEmits,
+} from "@vue/runtime-core";
+import { ref, computed } from "@vue/reactivity";
 const app = document.querySelector("#app");
 
 const props = defineProps({
@@ -38,9 +57,57 @@ const props = defineProps({
 
 const emit = defineEmits(["update:modelValue"]);
 
+const email = ref("");
+const password = ref("");
+const secondPassword = ref("");
+
 const handleClick = () => {
   emit("update:modelValue", false);
 };
+
+const handleSignUp = async () => {
+  try {
+    const result = await axios.post(
+      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${
+        import.meta.env.VITE_apiKey
+      }`,
+      { email: email.value, password: password.value, returnSecureToken: true }
+    );
+    if (result.status === 200) {
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Welcome~~",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: `Something shit happen`,
+      });
+    }
+    console.log(result);
+  } catch (err) {
+    Swal.fire({
+      icon: "error",
+      title: "Oops...",
+      text: `${err.response.data.error.message}，please try again!`,
+    });
+  }
+};
+
+const isPasswordCorrespond = computed(() => {
+  if (secondPassword.value === "" && password.value === "") return false;
+  return secondPassword.value !== password.value;
+});
+const isEmailValid = computed(() => {
+  if (email.value === "") return false;
+  return !email.value.match(
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  );
+});
 
 onMounted(() => {
   app.classList.add("blur");
--- src/main.js
+++ src/main.js
@@ -3,4 +3,5 @@ import App from './App.vue'
 import './index.css'
 import './scss/main.scss'
 import router from './router'
+import 'sweetalert2/src/sweetalert2.scss'
 createApp(App).use(router).mount('#app')
